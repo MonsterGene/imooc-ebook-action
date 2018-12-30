f5a5f3064d18ac36931b60020ecfca1191c1e777
MonsterGene <nkzj.2009@163.com>
2018-12-30 15:52:00 +0800

    正在学7-16

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,6 +1,9 @@
 <template>
 <div class="flap-card-wrapper" v-show="flapCardVisible">
-  <div class="flap-card-bg" :class="{'animation': runFlapCardAnimation}">
+  <div
+    class="flap-card-bg"
+    :class="{'animation': runFlapCardAnimation}"
+    v-show="runFlapCardAnimation">
     <div
       class="flap-card"
       v-for="(item, index) in flapCardList"
@@ -22,6 +25,22 @@
     </div>
   </div>
   <div
+    class="book-card"
+    :class="{'animation': runBookCardAnimation}"
+    v-show="runBookCardAnimation">
+    <div class="book-card-wrapper">
+      <div class="img-wrapper">
+        <img class="img" :src="data ? data.cover : ''">
+      </div>
+      <div class="content-wrapper">
+        <div class="content-title">{{ data ? data.title : '' }}</div>
+        <div class="content-author sub-title-medium">{{ data ? data.author : '' }}</div>
+        <div class="content-category">{{ categoryText() }}</div>
+      </div>
+      <div class="read-btn" @click.stop="showBookDetail(data)">{{ $t('home.readNow') }}</div>
+    </div>
+  </div>
+  <div
     class="close-btn-wrapper"
     >
     <span class="icon-close" @click="close"></span>
@@ -31,11 +50,14 @@
 
 <script>
 import { storeHomeMixin } from '../../utils/mixin'
-import { flapCardList } from '../../utils/store'
-import { setInterval, clearInterval, setTimeout } from 'timers'
+import { flapCardList, categoryText } from '../../utils/store'
+import { setInterval, clearInterval, setTimeout, clearTimeout } from 'timers'
 
 export default {
   mixins: [storeHomeMixin],
+  props: {
+    data: Object
+  },
   data () {
     return {
       flapCardList,
@@ -44,7 +66,8 @@ export default {
       intervalTime: 40,
       runFlapCardAnimation: false,
       pointList: null,
-      runPointAnimation: false
+      runPointAnimation: false,
+      runBookCardAnimation: false
     }
   },
   watch: {
@@ -133,15 +156,15 @@ export default {
         this.rotate(index, 'front')
         this.rotate(index, 'back')
       })
+      this.runBookCardAnimation = false
+      this.runFlapCardAnimation = false
+      this.runPointAnimation = false
     },
     startFlapCardAnimation () {
       this.prepare()
       this.task = setInterval(() => {
         this.flapCardRotate()
       }, this.intervalTime)
-      setTimeout(() => {
-        this.stopAnimation()
-      }, 3000)
     },
     startPointAnimation () {
       this.runPointAnimation = true
@@ -150,19 +173,31 @@ export default {
       }, 750)
     },
     stopAnimation () {
-      this.runFlapCardAnimation = false
       if (this.task) {
         clearInterval(this.task)
         this.task = null
       }
+      if (this.timeout) clearTimeout(this.timeout)
+      if (this.timeout2) clearTimeout(this.timeout2)
       this.reset()
     },
     runAnimation () {
       this.runFlapCardAnimation = true
-      setTimeout(() => {
+      this.timeout = setTimeout(() => {
         this.startFlapCardAnimation()
         this.startPointAnimation()
       }, 300)
+      this.timeout2 = setTimeout(() => {
+        this.stopAnimation()
+        this.runBookCardAnimation = true
+      }, 3000)
+    },
+    categoryText () {
+      if (this.data) {
+        return categoryText(this.data.category, this)
+      } else {
+        return ''
+      }
     }
   },
   created () {
@@ -249,6 +284,77 @@ export default {
             }
           }
         }
+      }
+    }
+  }
+  .book-card {
+    position: relative;
+    width: 65%;
+    max-width: px2rem(400);
+    box-sizing: border-box;
+    border-radius: px2rem(15);
+    background: white;
+    &.animation {
+      animation: scale .3s ease-in both;
+      @keyframes scale {
+        0% {
+          transform: scale(0);
+          opacity: 0;
+        }
+        100% {
+          transform: scale(1);
+          opacity: 1;
+        }
+      }
+    }
+    .book-card-wrapper {
+      width: 100%;
+      height: 100;
+      margin-bottom: px2rem(30);
+      @include columnTop;
+      .img-wrapper {
+        width: 100%;
+        margin-top: px2rem(20);
+        @include center;
+        .img {
+          width: px2rem(90);
+          height: px2rem(130);
+        }
+      }
+      .content-wrapper {
+        padding: 0 px2rem(20);
+        margin-top: px2rem(20);
+        .content-title {
+          color: #333;
+          font-weight: bold;
+          font-size: px2rem(18);
+          line-height: px2rem(20);
+          text-align: center;
+          @include ellipsis2(2);
+        }
+        .content-author {
+          margin-top: px2rem(10);
+          text-align: center;
+        }
+        .content-category {
+          color: #999;
+          font-size: px2rem(14);
+          margin-top: px2rem(10);
+          text-align: center;
+        }
+      }
+      .read-btn {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 1100;
+        width: 100%;
+        border-radius: 0 0 px2rem(15) px2rem(15);
+        padding: px2rem(15) 0;
+        text-align: center;
+        color: white;
+        font-size: px2rem(14);
+        background: $color-blue;
       }
     }
   }
--- src/views/store/StoreHome.vue
+++ src/views/store/StoreHome.vue
@@ -1,27 +1,11 @@
 <template>
 <div class="store-home">
   <search-bar></search-bar>
-  <flap-card></flap-card>
+  <flap-card :data="random"></flap-card>
   <scroll :top="scrollTop" @onScroll="onScroll" ref="scroll">
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
-    <p>金梅</p>
+    <div class="banner-wrapper">
+      <img class="banner-img" :src="banner" alt="" />
+    </div>
   </scroll>
 </div>
 </template>
@@ -31,13 +15,16 @@ import SearchBar from '../../components/home/SearchBar.vue'
 import Scroll from '../../components/common/Scroll.vue'
 import FlapCard from '../../components/home/FlapCard.vue'
 import { storeHomeMixin } from '../../utils/mixin'
+import { home } from '../../api/store'
 
 export default {
   mixins: [storeHomeMixin],
   components: { SearchBar, Scroll, FlapCard },
   data () {
     return {
-      scrollTop: 94
+      scrollTop: 94,
+      random: null,
+      banner: null
     }
   },
   methods: {
@@ -51,6 +38,17 @@ export default {
       }
       this.$refs.scroll.refresh()
     }
+  },
+  mounted () {
+    home().then(res => {
+      console.log(res)
+      if (res && res.status === 200) {
+        const data = res.data
+        const randomIndex = Math.floor(Math.random() * data.random.length)
+        this.random = data.random[randomIndex]
+        this.banner = data.banner
+      }
+    })
   }
 }
 </script>
@@ -60,5 +58,14 @@ export default {
 .store-home {
   width: 100%;
   height: 100%;
+  .banner-wrapper {
+    width: 100%;
+    padding: px2rem(10);
+    box-sizing: border-box;
+    .banner-img {
+      width: 100%;
+      height: px2rem(140);
+    }
+  }
 }
 </style>
